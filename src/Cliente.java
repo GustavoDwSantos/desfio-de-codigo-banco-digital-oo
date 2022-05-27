6f5c28913b30e3ddb67297f6de28762d4833d5a5
GustavoDwSantos <gustavos200116@gmail.com>
2022-05-27 13:33:03 -0300

    Criação de CPF e Idade para Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,13 +2,23 @@
 public class Cliente {
 
 	private String nome;
+	private String cpf;
+	private int idade;
+
+	public Cliente(String nome, String cpf, int idade ){
+		nome = this.nome;
+		cpf = this.cpf;
+		idade = this.idade;
+	}
 
 	public String getNome() {
 		return nome;
 	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
+	public String getCpf() {
+		return cpf;
+	}
+	public int getIdade() {
+		return idade;
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente gustavo = new Cliente("gustavo","111.111.111-47",18);
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(gustavo);
+		Conta poupanca = new ContaPoupanca(gustavo);
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
